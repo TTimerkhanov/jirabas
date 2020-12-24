4c3861d8b118c476d1a473ff1ea28893e8bd51dd
Timur Timerkhanov <timurgrunge@gmail.com>
2020-12-25 01:10:55 +0300

    :sparkles: The Dark Side of the Moon

--- jirabas/tasks/views.py
+++ jirabas/tasks/views.py
@@ -1,15 +1,23 @@
+from collections import defaultdict
+
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django_filters import rest_framework as filters
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from jirabas.tasks.models import Project, ProjectMembership, Task
+from jirabas.tasks.enums import RelationType
+from jirabas.tasks.models import Project, ProjectMembership, Task, TasksRelation
 from jirabas.tasks.serializers import (
     ProjectRoleSerializer,
     ProjectSerializer,
+    ProjectUserSerializer,
     TaskSerializer,
+    TasksRelationCategoriesSerializer,
 )
-from jirabas.users.models import Role, User
+from jirabas.users.serializers import UserProjectInfoSerializer
 
 
 class ProjectViewSet(ModelViewSet):
@@ -20,21 +28,94 @@ class ProjectViewSet(ModelViewSet):
         return Project.objects.filter(members__member=self.request.user)
 
     @action(detail=True, methods=["post"], serializer_class=ProjectRoleSerializer)
-    def add_users(self, request, pk=None):
+    def add_user(self, request, pk=None):
+        serializer = ProjectRoleSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        project = self.get_object()
+
+        try:
+            ProjectMembership.objects.create(
+                project=project,
+                member_id=serializer.data["user"],
+                role_id=serializer.data["role"],
+            )
+        except IntegrityError:
+            return JsonResponse(
+                data={"error": "Пользователь уже добавлен к проекту"},
+                status=status.HTTP_201_CREATED,
+            )
+
+        return Response(status=status.HTTP_201_CREATED)
+
+    @action(detail=True, methods=["post"], serializer_class=ProjectRoleSerializer)
+    def change_role(self, request, pk=None):
         project = self.get_object()
         serializer = ProjectRoleSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        role = Role.objects.get(pk=serializer.data["role"])
-        users = User.objects.filter(pk__in=serializer.data["users"])
-        objects = [
-            ProjectMembership(project=project, member=user, role=role) for user in users
-        ]
-        ProjectMembership.objects.bulk_create(objects)
+        ProjectMembership.objects.filter(
+            project=project, member_id=serializer.data["user"]
+        ).update(role_id=serializer.data["role"])
 
-        return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=["post"], serializer_class=ProjectUserSerializer)
+    def remove_user(self, request, pk=None):
+        project = self.get_object()
+        serializer = ProjectUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        ProjectMembership.objects.filter(
+            project=project, member_id=serializer.data["user"]
+        ).delete()
+        return Response(status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=["get"], serializer_class=UserProjectInfoSerializer)
+    def users(self, request, pk=None):
+        membership = ProjectMembership.objects.filter(
+            project_id=self.kwargs[self.lookup_field]
+        ).select_related("member", "role")
+        data = [
+            {
+                "id": entry.member.id,
+                "name": entry.member.name,
+                "username": entry.member.username,
+                "email": entry.member.email,
+                "roles": entry.role.name,
+            }
+            for entry in membership
+        ]
+
+        return JsonResponse(data={"results": data})
 
 
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ("performer", "project")
+
+    @action(
+        detail=True, methods=["get"], serializer_class=TasksRelationCategoriesSerializer
+    )
+    def related(self, request, pk=None):
+        task = self.get_object()
+        data = defaultdict(list)
+
+        from_relations = TasksRelation.objects.filter(from_task=task).select_related(
+            "to_task"
+        )
+        for rel in from_relations:
+            data[RelationType(rel.relation_type).label].append(rel.to_task)
+
+        to_relations = TasksRelation.objects.filter(to_task=task).select_related(
+            "from_task"
+        )
+        for rel in to_relations:
+            data[RelationType(rel.relation_type).label].append(rel.from_task)
+
+        transformed_data = [{"relation_type": k, "tasks": v} for k, v in data.items()]
+
+        data = TasksRelationCategoriesSerializer({"relations": transformed_data}).data
+        return JsonResponse(data={"results": data})
--- jirabas/users/views.py
+++ jirabas/users/views.py
@@ -1,11 +1,22 @@
 from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from jirabas.users.serializers import UserSerializer
+from jirabas.tasks.models import Project, ProjectMembership
+from jirabas.users.models import Role
+from jirabas.users.serializers import (
+    RoleSerializer,
+    UserProjectInfoSerializer,
+    UserSerializer,
+    UserShortInfoSerializer,
+)
 
 User = get_user_model()
 
@@ -25,7 +36,48 @@ class UserViewSet(ModelViewSet):
     def get_queryset(self, *args, **kwargs):
         return self.queryset.filter(id=self.request.user.id)
 
+    @swagger_auto_schema(responses={status.HTTP_200_OK: UserShortInfoSerializer()})
     @action(detail=False, methods=["GET"])
     def me(self, request):
-        serializer = UserSerializer(request.user, context={"request": request})
+        serializer = UserShortInfoSerializer(request.user, context={"request": request})
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                "project",
+                openapi.IN_QUERY,
+                description="Project ID",
+                required=True,
+                type=openapi.TYPE_STRING,
+            ),
+        ],
+        responses={status.HTTP_200_OK: UserProjectInfoSerializer()},
+    )
+    @action(detail=False, methods=["GET"])
+    def project_role(self, request):
+        if not request.query_params.get("project"):
+            return JsonResponse(
+                data={"error": "Заполните параметр project"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        project = get_object_or_404(Project, pk=request.query_params["project"])
+        membership = get_object_or_404(
+            ProjectMembership, member=request.user, project=project
+        )
+
+        serializer = UserProjectInfoSerializer(
+            {
+                "username": request.user.username,
+                "email": request.user.email,
+                "name": request.user.name,
+                "role": membership.role.name,
+            }
+        )
+        return JsonResponse(status=status.HTTP_200_OK, data=serializer.data)
+
+
+class RoleViewSet(ModelViewSet):
+    serializer_class = RoleSerializer
+    queryset = Role.objects.all()
